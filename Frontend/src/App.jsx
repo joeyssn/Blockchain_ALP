51aad8df6a989d43b723122784b8ea788f0c1507
RickykennardW <rkennard@student.ciputra.ac.id>
2026-06-07 16:19:40 +0700

    UI FIX

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ErrorMessage } from "./components/ErrorMessage.jsx";
 import { Navbar } from "./components/Navbar.jsx";
