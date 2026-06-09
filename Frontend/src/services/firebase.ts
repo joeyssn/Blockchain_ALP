83bbb5a18e8c614ef61786ce21327f4c1bf6de4a
RickykennardW <153189273+RickykennardW@users.noreply.github.com>
2026-06-09 13:16:06 +0700

    Debug Firebase Fix 1.0

--- Frontend/src/services/firebase.ts
+++ Frontend/src/services/firebase.ts
@@ -3,14 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "...",
+  apiKey: "AIzaSyBuUih0zROYc7L8GFEAwV3vz_hnU5w6zSI",
   authDomain: "blockchain-bda23.firebaseapp.com",
   projectId: "blockchain-bda23",
   storageBucket: "blockchain-bda23.firebasestorage.app",
   messagingSenderId: "372628889016",
   appId: "1:372628889016:web:4ad29f8dfd1f8064ca034d",
+  measurementId: "G-JFDFHR0BHX",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export const storage = getStorage(firebaseApp);
+
+export default firebaseApp;
